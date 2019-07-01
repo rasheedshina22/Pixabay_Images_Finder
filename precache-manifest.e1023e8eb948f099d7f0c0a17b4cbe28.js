2aacde1eed662ce17adfd5df42d79e90203fce94
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2019-07-01 13:24:06 -0700

    Updates

---
+++ precache-manifest.e1023e8eb948f099d7f0c0a17b4cbe28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f40868c688d521e1231d",
+    "url": "/Pixabay_Images_Finder/static/css/main.bc22f991.chunk.css"
+  },
+  {
+    "revision": "f40868c688d521e1231d",
+    "url": "/Pixabay_Images_Finder/static/js/main.14ab89ab.chunk.js"
+  },
+  {
+    "revision": "94b3a7c1ffbfed7f9cd5",
+    "url": "/Pixabay_Images_Finder/static/js/runtime~main.7f0de0b9.js"
+  },
+  {
+    "revision": "9712a1a22cf18329bb45",
+    "url": "/Pixabay_Images_Finder/static/js/2.7806a350.chunk.js"
+  },
+  {
+    "revision": "1ded91f97722affbd02e60eda6664244",
+    "url": "/Pixabay_Images_Finder/index.html"
+  }
+];
